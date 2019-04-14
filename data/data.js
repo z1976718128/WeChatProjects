39f0b768427b76da3fae7fd3a0be302d00d5103c
unknown <unknown@example.com>
2019-04-14 08:07:30 +0800

    Initial Commit

---
+++ data/data.js
@@ -0,0 +1,93 @@
+var dataJson =[
+ [
+    {
+      "id": 1,
+      "imgs": "https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=321129133,2078619143&fm=26&gp=0.jpg"
+    },
+    {
+      "id": 2,
+      "imgs": "https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=3521473536,3994351560&fm=26&gp=0.jpg"
+    },
+    {
+      "id": 3,
+      "imgs": "https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=3996333838,1164261288&fm=26&gp=0.jpg"
+    },
+    {
+      "id": 4,
+      "imgs": "https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=1738816357,3984113258&fm=26&gp=0.jpg"
+    }
+  ],
+  [
+    {
+      "id":1,
+      "text":"您好,欢迎进入解忧食客铺"
+    },
+    {
+      "id": 2,
+      "text": "进店可领取优惠"
+    },
+    {
+      "id": 3,
+      "text": "更有拼团、砍价等优惠活动"
+    },
+    {
+      "id": 4,
+      "text": "全场满100元包邮"
+    }
+  ],
+  [
+    {
+      "id": 1,
+      "imgs": "https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=2849150988,2367507371&fm=26&gp=0.jpg",
+      "text": "大闸蟹拼盘",
+      "price":65,
+      "sold":50,
+      "oddPrice":78
+    },
+    {
+      "id": 2,
+      "imgs": "https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=2662973399,2232806155&fm=26&gp=0.jpg",
+      "text": "海鲜品牌",
+      "price": 45,
+      "sold": 60,
+      "oddPrice": 60
+    },
+    {
+      "id": 3,
+      "imgs": "https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=4172684262,1155354722&fm=26&gp=0.jpg",
+      "text": "皮皮虾",
+      "price": 40,
+      "sold": 50,
+      "oddPrice": 65
+    },
+    {
+      "id": 4,
+      "imgs": "https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=44977444,1057896112&fm=26&gp=0.jpg",
+      "text": "鲜鲜生蚝",
+      "price": 39,
+      "sold": 70,
+      "oddPrice": 50
+    },
+    {
+      "id": 5,
+      "imgs": "https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=3390078393,467662584&fm=26&gp=0.jpg",
+      "text": "卤香猪蹄",
+      "price": 25,
+      "sold": 50,
+      "oddPrice": 30
+    },
+    {
+      "id": 6,
+      "imgs": "https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=1984865428,435804210&fm=26&gp=0.jpg",
+      "text": "香辣藕片",
+      "price": 23,
+      "sold": 80,
+      "oddPrice": 28
+    },
+    
+  ]
+
+]
+module.exports ={
+  dataList:dataJson
+}
---
+++ pages/home/home.js
@@ -0,0 +1,80 @@
+//home.js
+//导出数据
+const data = require('../../data/data.js')
+
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    
+  },
+  goShops:function(e){
+    // console.log(e)
+    var shopId = e.currentTarget.dataset.id
+     wx.navigateTo({
+       url: '/pages/shops/shops?id='+shopId,
+     })
+  },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    this.setData({
+      dataList: data.dataList[0],
+      news: data.dataList[1],
+      Acrab: data.dataList[2],
+
+    }) 
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+    
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+    
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+    
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+    
+  }
+})
